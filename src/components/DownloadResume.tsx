80925b53437b730ac661cac6ae52530278df10e3
Matt <mattkbrat@pm.me>
2023-07-01 12:09:10 -0600

    Remove unused code

--- src/components/DownloadResume.tsx
+++ src/components/DownloadResume.tsx
@@ -1,18 +1,15 @@
-async function downloadFile(url: string, filename: string) {
-    console.log('Downloading file: ' + filename);
-    const response = await fetch(url);
-    const blob = await response.blob();
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = filename;
-    link.click();
- }
-
 export default function DownloadResume() {
      return (
         <a
             href='Resume.pdf'
             download="Resume - Matthew Bratrsovsky.pdf"
+            style={{
+                color: 'white',
+                textDecoration: 'none',
+                backgroundColor: '#2c3e50',
+                padding: '10px 20px',
+                marginRight: '4rem',
+            }}
         >
             Download Resume
         </a>
