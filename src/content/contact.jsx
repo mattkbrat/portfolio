3ba77e015a34d45638d915c0939abaac7da2c5b9
Matt <mattkbrat@pm.me>
2023-07-01 12:42:25 -0600

    Update styles with a focus on legibility

--- src/content/contact.jsx
+++ src/content/contact.jsx
@@ -1,16 +1,27 @@
 export default function (){
     return (
         <>
-        <p>
+        <p
+            style={{
+                fontSize: "1.2rem",
+                textAlign: "center",
+                textTransform: "uppercase",
+                textUnderlineOffset: "0.2rem",
+                }}
+        >
         {}
         <a href={`mailto:mattkbrat@pm.me`}>
         Contact Me
         </a>
-        {" | "}{}
+        {" ⚈ "}{}
         <a href={`https://www.linkedin.com/in/mattkbrat/`}>
         LinkedIn
-        </a> {" | Colorado, USA"}
+        </a>
+        {" ⚈ "}{}
+        <a href={`https://www.github.com/mattkbrat/`}>
+        GitHub
+        </a> {" ⚈ Colorado, USA"}
         </p>
-</>
+        </>
     )
 }
