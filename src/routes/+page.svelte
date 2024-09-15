8ade051a33a2d2f6bb7570722b0ee2a3fd38a421
Matthew Bratrsovsky <realzing@pm.me>
2024-09-15 13:41:46 -0600

    feat: load the resume content on the server page load
    Fixes the issue of needing the client to do the fetch + loads the
    content on page load.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,167 +1,161 @@
 <script lang="ts">
-import { browser } from "$app/environment";
-import { goto, replaceState } from "$app/navigation";
-import { page } from "$app/stores";
-import SvelteMarkdown from "$lib/SvelteMarkdown.svelte";
-import TableOfContents from "$lib/TableOfContents.svelte";
-import {
-	changePosition,
-	headings,
-	minChangeDiff,
-	position,
-	positionManager,
-} from "$lib/stores/content";
-import { onMount } from "svelte";
+	import { browser } from '$app/environment';
+	import { goto, replaceState } from '$app/navigation';
+	import { page } from '$app/stores';
+	import SvelteMarkdown from '$lib/SvelteMarkdown.svelte';
+	import TableOfContents from '$lib/TableOfContents.svelte';
+	import {
+		changePosition,
+		headings,
+		minChangeDiff,
+		position,
+		positionManager
+	} from '$lib/stores/content';
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
 
-let resumeContent = "";
+	export let data: PageData;
 
-let frontmatter = "";
-let hasLoaded: boolean | string = false;
+	let resumeContent = '';
 
-let visibilities: { id: string; visible: boolean }[] = [];
+	let frontmatter = '';
+	let hasLoaded: boolean | string = false;
 
-const updateSelectionClasses = () => {
-	if (!browser) return;
-	const e = document.getElementById($position);
-	if (e) {
-		e.classList.add("selected-heading");
-	}
+	let visibilities: { id: string; visible: boolean }[] = [];
 
-	for (const heading of $headings.filter((h) => h.id !== $position)) {
-		if (!heading || !heading.id) {
-			// console.warn('Invalid heading', heading);
-			return;
-		}
-		const e = document.getElementById(heading.id);
+	const updateSelectionClasses = () => {
+		if (!browser) return;
+		const e = document.getElementById($position);
 		if (e) {
-			// console.log('unselecting', heading, $position);
-			e.classList.remove("selected-heading");
-		} else {
-			// console.log('No element by id', heading.id);
+			e.classList.add('selected-heading');
+		}
+
+		for (const heading of $headings.filter((h) => h.id !== $position)) {
+			if (!heading || !heading.id) {
+				// console.warn('Invalid heading', heading);
+				return;
+			}
+			const e = document.getElementById(heading.id);
+			if (e) {
+				// console.log('unselecting', heading, $position);
+				e.classList.remove('selected-heading');
+			} else {
+				// console.log('No element by id', heading.id);
+			}
+		}
+	};
+
+	$: if (visibilities && hasLoaded) {
+		const lastVisible = visibilities.find((v) => v.visible);
+		if (
+			lastVisible &&
+			((typeof hasLoaded === 'string' && lastVisible.id !== hasLoaded) || !!hasLoaded)
+		) {
+			const now = new Date().getTime();
+			if (now - $positionManager.lastChanged > minChangeDiff) {
+				const lastVisibleId = lastVisible.id;
+				const asHref = `/#${lastVisibleId}`;
+				replaceState(asHref, '');
+				changePosition(lastVisibleId, $positionManager);
+			}
+			updateSelectionClasses();
 		}
 	}
-};
 
-$: if (visibilities && hasLoaded) {
-	const lastVisible = visibilities.find((v) => v.visible);
-	if (
-		lastVisible &&
-		((typeof hasLoaded === "string" && lastVisible.id !== hasLoaded) ||
-			!!hasLoaded)
+	function onVisible(
+		element: HTMLElement,
+		callback: ({ id, status }: { id: string; status: boolean }) => void
 	) {
-		const now = new Date().getTime();
-		if (now - $positionManager.lastChanged > minChangeDiff) {
-			const lastVisibleId = lastVisible.id;
-			const asHref = `/#${lastVisibleId}`;
-			replaceState(asHref, "");
-			changePosition(lastVisibleId, $positionManager);
-		}
-		updateSelectionClasses();
+		visibilities.push({
+			id: element.id,
+			visible: false
+		});
+		new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				const index = visibilities.findIndex((v) => v.id === element.id);
+				const isVisible = entry.intersectionRatio > 0;
+				visibilities[index].visible = isVisible;
+			});
+		}).observe(element);
+		if (!callback) return new Promise((r) => (callback = r));
 	}
-}
 
-function onVisible(
-	element: HTMLElement,
-	callback: ({ id, status }: { id: string; status: boolean }) => void,
-) {
-	visibilities.push({
-		id: element.id,
-		visible: false,
-	});
-	new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-			const index = visibilities.findIndex((v) => v.id === element.id);
-			const isVisible = entry.intersectionRatio > 0;
-			visibilities[index].visible = isVisible;
-		});
-	}).observe(element);
-	if (!callback) return new Promise((r) => (callback = r));
-}
+	// $: if ($page.url.hash) {
+	// 	setSelected($page.url.hash.slice(1));
+	// }
 
-// $: if ($page.url.hash) {
-// 	setSelected($page.url.hash.slice(1));
-// }
+	$: if (resumeContent.length > 0 && $headings.length === 0) {
+		setTimeout(() => {
+			const docHeadings = document.getElementsByTagName('h2');
 
-$: if (resumeContent.length > 0 && $headings.length === 0) {
-	setTimeout(() => {
-		const docHeadings = document.getElementsByTagName("h2");
+			const asArray = Array.from(docHeadings).filter((h) => h.id);
 
-		const asArray = Array.from(docHeadings).filter((h) => h.id);
+			const profileLinks = document.getElementById('profile-links');
 
-		const profileLinks = document.getElementById("profile-links");
+			if (profileLinks instanceof HTMLHeadingElement) {
+				asArray.unshift(profileLinks);
+			}
+			// asArray.push("profile-links");
+			for (const heading of asArray) {
+				onVisible(heading, () => {});
+			}
 
-		if (profileLinks instanceof HTMLHeadingElement) {
-			asArray.unshift(profileLinks);
+			headings.set(
+				asArray
+					.map((h) => {
+						return {
+							heading: h.innerText.length < 10 ? h.innerText : h.id,
+							href: `#${h.id}`,
+							id: h.id
+						};
+					})
+					.filter(Boolean)
+			);
+			const { hash } = window.location;
+			if (hash) {
+				const id = hash.slice(1);
+				changePosition(id, $positionManager, true);
+				const element = document.getElementById(hash.slice(1));
+				element?.scrollIntoView();
+				hasLoaded = id;
+			}
+		}, 150);
+	}
+
+	const styleAnchors = () => {
+		const anchors = Array.from(document.getElementsByTagName('a')) as HTMLAnchorElement[];
+
+		for (const a of anchors.filter(
+			(a) => (a.href.includes('https') || a.href.includes('mailto')) && a.id !== 'github-corner'
+		)) {
+			a.classList.add('external-link');
+			a.target = '_blank';
 		}
-		// asArray.push("profile-links");
-		for (const heading of asArray) {
-			onVisible(heading, () => {});
-		}
+	};
 
-		headings.set(
-			asArray
-				.map((h) => {
-					return {
-						heading: h.innerText.length < 10 ? h.innerText : h.id,
-						href: `#${h.id}`,
-						id: h.id,
-					};
-				})
-				.filter(Boolean),
+	$: if (!$page.url.hash && browser) {
+		goto('#profile-links', { replaceState: true });
+	}
+
+	$: if (data.content && browser) {
+		const [, properties, ...rest] = data.content.split('---');
+
+		frontmatter = properties.trim().split('\n').join('\n\n');
+		resumeContent = rest.join('\n');
+
+		styleAnchors();
+	}
+
+	onMount(() => {
+		console.log(
+			'%cWell, Howdy! : - )',
+			'color: blue; font-family: sans-serif; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;'
 		);
-		const { hash } = window.location;
-		if (hash) {
-			const id = hash.slice(1);
-			changePosition(id, $positionManager, true);
-			const element = document.getElementById(hash.slice(1));
-			element?.scrollIntoView();
-			hasLoaded = id;
-		}
-	}, 150);
-}
-
-const styleAnchors = () => {
-	const anchors = Array.from(
-		document.getElementsByTagName("a"),
-	) as HTMLAnchorElement[];
-
-	anchors
-		.filter(
-			(a) =>
-				(a.href.includes("https") || a.href.includes("mailto")) &&
-				a.id !== "github-corner",
-		)
-		.forEach((a) => {
-			a.classList.add("external-link");
-			a.target = "_blank";
+		window.addEventListener('hashchange', () => {
+			const { hash } = window.location;
+			changePosition(hash.slice(1), $positionManager, true);
 		});
-};
-
-$: if (!$page.url.hash && browser) {
-	goto("#profile-links", { replaceState: true });
-}
-
-onMount(() => {
-	console.log(
-		"%cWell, Howdy! : - )",
-		"color: blue; font-family: sans-serif; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;",
-	);
-	window.addEventListener("hashchange", () => {
-		const { hash } = window.location;
-		changePosition(hash.slice(1), $positionManager, true);
 	});
-
-	fetch("/resume.md")
-		.then(async (res) => {
-			const allContent = await res.text();
-
-			const [, properties, ...content] = allContent.split("---");
-
-			frontmatter = properties.trim().split("\n").join("\n\n");
-			resumeContent = content.join("\n");
-		})
-		.then(styleAnchors);
-});
 </script>
 
 <svelte:head>
