665ef31752b05ab22f8f5d0185efe253c938622c
Matthew Bratrsovsky <realzing@pm.me>
2024-12-07 12:07:59 -0700

    refactor(styling): use the vintage skeleton theme
    Yummy Orange

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,33 @@
-import { join } from 'path';
-import type { Config } from 'tailwindcss';
-import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from "path";
+import type { Config } from "tailwindcss";
+import { skeleton } from "@skeletonlabs/tw-plugin";
 
 export default {
-	darkMode: 'class',
+	darkMode: "class",
 	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+		"./src/**/*.{html,js,svelte,ts}",
+		join(
+			require.resolve("@skeletonlabs/skeleton"),
+			"../**/*.{html,js,svelte,ts}",
+		),
 	],
 	theme: {
 		extend: {
 			animation: {
-				'bounce-once': 'bounce 1s ease-in-out 1'
-			}
-		}
+				"bounce-once": "bounce 1s ease-in-out 1",
+			},
+		},
 	},
 	plugins: [
 		skeleton({
 			themes: {
 				preset: [
 					{
-						name: 'modern',
-						enhancements: true
+						name: "vintage",
+						enhancements: true,
 					},
-					{
-						name: 'sahara',
-						enhancements: true
-					},
-					{
-						name: 'vintage',
-						enhancements: true
-					}
-				]
-			}
-		})
-	]
+				],
+			},
+		}),
+	],
 } satisfies Config;
