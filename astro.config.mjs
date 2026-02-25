ef21e772bef0bb2cd3f66e4ab207ad2ba87cf9fd
Matthew Bratrsovsky <matt@solutionsartisan.com>
2026-02-25 13:40:13 -0700

    feat: deploy resume with astro and starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  redirects: {
+    "/": "/resume",
+  },
+  integrations: [
+    starlight({
+      pagefind: false,
+      title: "Matthew Bratrsovsky",
+      customCss: ["./src/styles/main.css"],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/mattkbrat",
+        },
+      ],
+    }),
+  ],
 });
