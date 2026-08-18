501c79a5099821f875bedc396bc09447202fd6d6
Matthew Bratrsovsky <77427123+mattkbrat@users.noreply.github.com>
2026-08-18 16:54:12 -0600

    Feat/smpp project (#10)
    add details on smpp project including mermaid diagrams, page, and popover

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,33 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mattkbrat.com/resume",
   redirects: {
     "/": "/resume",
   },
-  integrations: [
-    starlight({
-      pagefind: false,
-      title: "Matthew Bratrsovsky",
-      customCss: ["./src/styles/main.css"],
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/mattkbrat",
-        },
-      ],
-      components: {
-        Hero: "./src/components/hero.astro",
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [starlight({
+    pagefind: false,
+    title: "Matthew Bratrsovsky",
+    customCss: ["./src/styles/main.css"],
+    social: [
+      {
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/mattkbrat",
       },
-    }),
-  ],
+    ],
+    components: {
+      Hero: "./src/components/hero.astro",
+    },
+  }),
+  sitemap()],
 });
