efbbc6a4bb4db72784de6535a3fa4614aba30f27
Matthew Bratrsovsky <matt@solutionsartisan.com>
2026-08-18 16:41:16 -0600

    move from fetching url to loading raw

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,29 +3,31 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mattkbrat.com/resume",
   redirects: {
     "/": "/resume",
   },
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [
-    starlight({
-      pagefind: false,
-      title: "Matthew Bratrsovsky",
-      customCss: ["./src/styles/main.css"],
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/mattkbrat",
-        },
-      ],
-      components: {
-        Hero: "./src/components/hero.astro",
+  integrations: [starlight({
+    pagefind: false,
+    title: "Matthew Bratrsovsky",
+    customCss: ["./src/styles/main.css"],
+    social: [
+      {
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/mattkbrat",
       },
-    }),
-  ],
+    ],
+    components: {
+      Hero: "./src/components/hero.astro",
+    },
+  }),
+  sitemap()],
 });
